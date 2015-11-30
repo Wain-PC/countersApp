d07f0829bb1a72b1fc9beb5e3f6a2c22cafd70b9
Pavel Dranichnikov <dranichnikov@teletrade.ru>
2015-11-30 19:51:53 +0300

    Создаем админку - интерфейс для разбора почты

---
+++ client/templates/_admin/postParse/postParse.js
@@ -0,0 +1,21 @@
+AutoForm.addHooks('adminUseraddForm', AutoFormHooks.getServerMessage);
+
+Template.adminPostparse.events({
+    'click button#postParse': function (event) {
+        event.preventDefault();
+        var button = $(event.target);
+        button.attr('disabled','disabled');
+        console.log("Btn disabled");
+        var generatedPassword = Meteor.call('adm_checkMail', function (error, result) {
+            if(error || result.error) {
+                Notifications.error('Ошибка обработки почты', error || result.error, {timeout: 7000});
+            }
+            else {
+                Notifications.success('Почта обработана успешно',
+                    "Получено "+ result.result + " новых сообщений",
+                    {timeout: 5000});
+            }
+            button.removeAttr('disabled');
+        });
+    }
+});
